3001e957e60c2977c536211f68261a8b20c9c1d1
JEONG-JI-HEON <wlgjs0804@naver.com>
2022-11-25 11:29:47 +0900

    정지헌

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -2,6 +2,7 @@ var swiper = new Swiper(".mySwiper", {
   direction: "vertical",
   slidesPerView: 1,
   spaceBetween: 0,
+  speed: 1200,
   mousewheel: true,
   pagination: {
     el: ".swiper-pagination",
