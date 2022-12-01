f3b6f14417cd208853bd2c136072b443c4948029
JEONG-JI-HEON <wlgjs0804@naver.com>
2022-12-01 10:40:57 +0900

    첫번째 섹션 반응형 제외 완료

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -5,31 +5,31 @@ var swiper = new Swiper(".mySwiper", {
   spaceBetween: 0,
   speed: 800,
   mousewheel: true,
-  allowTouchMove : false,
+  allowTouchMove: false,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
 });
 
-  // 첫번째 섹션의 swiper
-  var swiper = new Swiper(".mySwiper1", {
-    // spaceBetween: 30,
-    effect: "fade",
-    pagination: {
-      el: ".swiper-pagination1",
-      clickable: true,
-      renderBullet: function (index, className) {
-        // return '<span class="' +  className + '">' + (index + 1) + "</span>";
-        return `<span class=${className}> 0${index + 1} </span>`
-      },
+// 첫번째 섹션의 swiper
+var swiper = new Swiper(".mySwiper1", {
+  // spaceBetween: 30,
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination1",
+    clickable: true,
+    renderBullet: function (index, className) {
+      // return '<span class="' +  className + '">' + (index + 1) + "</span>";
+      return `<span class=${className}> 0${index + 1} </span>`
     },
-    // autoplay: {
-    //   delay: 5000,
-    //   // disableOnInteraction: false,
-    // },
-    allowTouchMove : false,
-  });
+  },
+  // autoplay: {
+  //   delay: 5000,
+  //   // disableOnInteraction: false,
+  // },
+  allowTouchMove: false,
+});
 
 
-  // 두번째 섹션의 swiper
+// 두번째 섹션의 swiper
