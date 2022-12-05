b09b3788fb40f55b8b9a91f83ef94d2a9b84af3f
JinHaGyung <wlsgkrud96@gmail.com>
2022-12-05 13:10:59 +0900

    풋터 반응형800

--- routes/route.js
+++ routes/route.js
@@ -6,5 +6,8 @@ const { title } = require('process');
 router.get('/', (req, res)=>{ /* 메인페이지 */
   res.render('mainPage');
 });
+router.get('/footer', (req, res)=>{ /* 픗터확인용 */
+  res.render('footer');
+});
 
 module.exports = router;
