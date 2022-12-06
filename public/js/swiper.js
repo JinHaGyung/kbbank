2b59bd921e783686f8eb6bf4fac61cab055b2484
JEONG-JI-HEON <wlgjs0804@naver.com>
2022-12-06 09:53:55 +0900

    반응형(1200)됐을때 스크롤메인스와이퍼 작동 onoff기능 구현했습니다

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -1,24 +1,65 @@
-// 스크롤 했을때의 swiper
-var swiper = new Swiper(".mySwiper", {
-  direction: "vertical",
-  slidesPerView: 1,
-  spaceBetween: 0,
-  speed: 800,
-  mousewheel: true,
-  allowTouchMove: false,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    renderBullet: function (index, className) {
-      return `
-      <div class="${className}" id="${className}${index}">
-        <span class="navBtn"></span>
-        <span class="navTxt"></span>
-      </div>
-      `;
-    },
-  },
+var nowWidth = window.innerWidth;
+var swiper0 = undefined;
+
+function initSwiper() {
+  if (nowWidth >= 1200 && swiper0 == undefined) {
+    swiper0 = new Swiper(".mySwiper", {
+      direction: "vertical",
+      slidesPerView: 1,
+      spaceBetween: 0,
+      speed: 800,
+      mousewheel: true,
+      allowTouchMove: false,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+          return `
+          <div class="${className}" id="${className}${index}">
+            <span class="navBtn"></span>
+            <span class="navTxt"></span>
+          </div>
+          `;
+        },
+      },
+    });
+
+    // 왼쪽 내비 관련 스크립트
+    let swiperBtn0 = document.querySelector('#swiper-pagination-bullet0>.navTxt');
+    let swiperBtn1 = document.querySelector('#swiper-pagination-bullet1>.navTxt');
+    let swiperBtn2 = document.querySelector('#swiper-pagination-bullet2>.navTxt');
+    let swiperBtn3 = document.querySelector('#swiper-pagination-bullet3>.navTxt');
+
+    swiperBtn0.innerText = 'With KB';
+    swiperBtn1.innerText = 'KB저축은행 솔루션';
+    swiperBtn2.innerText = 'KB  Info';
+    swiperBtn3.innerText = '저축은행 공지사항';
+    
+  } else if (nowWidth < 1200 && swiper0 != undefined) {
+    swiper0.destroy();
+    swiper0 = undefined;
+
+    // 남아있는 페이지네이션 없애기
+    function removePageNav() {
+      let pageNation = document.querySelector('.swiper>.swiper-pagination');
+      while (pageNation.firstChild) {
+        pageNation.removeChild(pageNation.firstChild);
+      };
+    };
+    removePageNav();
+  };
+};
+
+initSwiper();
+
+window.addEventListener('resize', function () {
+  nowWidth = window.innerWidth;
+  initSwiper();
+  // swiper0 = undefined;
+
 });
+
+
 
 
 // 첫번째 섹션(mainSlide)의 swiper
@@ -40,7 +81,7 @@ var swiper1 = new Swiper(".mySwiper1", {
 });
 
 // 세번째 섹션(kbinfo)의 swiper
-var swiper = new Swiper(".mySwiper2", {
+var swiper2 = new Swiper(".mySwiper2", {
   slidesPerView: 2,
   spaceBetween: 30,
   slidesPerGroup: 1,
@@ -55,7 +96,7 @@ var swiper = new Swiper(".mySwiper2", {
   },
 });
 
-var swiper = new Swiper(".mySwiper3", {
+var swiper3 = new Swiper(".mySwiper3", {
   slidesPerView: 2,
   spaceBetween: 30,
   slidesPerGroup: 1,
@@ -70,7 +111,7 @@ var swiper = new Swiper(".mySwiper3", {
   },
 });
 
-var swiper = new Swiper(".mySwiper4", {
+var swiper4 = new Swiper(".mySwiper4", {
   slidesPerView: 4,
   spaceBetween: 0,
   // slidesPerGroup: 1,
