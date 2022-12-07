d0bf90450a2db47c363cb5c32c7cfc4b3b1a398e
JinHaGyung <wlsgkrud96@gmail.com>
2022-12-07 12:29:23 +0900

    풋터 셀렉트 아이디 추가

--- routes/route.js
+++ routes/route.js
@@ -14,4 +14,16 @@ router.get('/sub_noticeList', (req, res)=>{ /* 공지사항 리스트 */
   res.render('sub_noticeList');
 });
 
+
+router.get('/notice_write', (req, res)=>{ /* 공지사항 작성 */
+  res.render('notice_write');
+});
+
+
+router.get('/notice_correct', (req, res)=>{ /* 공지사항 수정 */
+  res.render('notice_correct');
+});
+
+
+
 module.exports = router;
