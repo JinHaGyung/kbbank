d6f4498f10d6da683e88ab0783d2a1b18a540950
JinHaGyung <wlsgkrud96@gmail.com>
2022-12-09 11:23:53 +0900

    게시판 작성페이지

---
+++ public/js/notice.js
@@ -0,0 +1,41 @@
+// 작성 공란 검사
+function notice_ck(){ 
+  var writeWriter= document.getElementById('write_writer'); 
+  var writeTitle = document.getElementById('write_title'); 
+  var writeCont = document.getElementById('write_cont'); 
+
+  if (writeWriter.value==''){
+    alert("작성자 이름은 비워둘 수 없습니다.");
+    writeWriter.focus();
+    return false;
+  }
+  if (writeTitle.value==''){
+    alert("제목을 비워둘 수 없습니다.");
+    writeTitle.focus();
+    return false;
+  }
+  if (writeCont.value==''){
+    alert("내용을 비워둘 수 없습니다.");
+    writeCont.focus();
+    return false;
+  }
+} 
+// radio 필수 선택
+<Box onClick={() => {
+	window.location.href = 'https://www.naver.com';
+	}}
+>
+const writeUrl
+
+window.open()
+function radioBtn(){
+  var writeRadio = document.querySelector(".write_radio");
+  var chk = document.getElementsByName("r_chkall");
+  if(frm.chk.checked){
+    frm.agree[0].checked;
+    frm.agree2[0].checked;
+  }
+}
+// 예약업로드 선택시 date 활성화
+
+// 예약 업로드 선택시 예약된 시간에 업로드되기
