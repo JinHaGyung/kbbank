58d71836cf415a9a72b62a8b7331c061431ddd79
daeuniJ <daeunjeong161@gmail.com>
2022-12-12 16:21:50 +0900

    정다은_서브페이지 반응형

--- public/js/faqPage.js
+++ public/js/faqPage.js
@@ -42,3 +42,27 @@ for(i=0; i<faq_accor.length; i++) {
         }
     });
 }
+
+
+// TOP버튼
+let topBtn = document.querySelector('#topBtn');
+
+function scrollBtn() {
+    if(document.body.scrollTop > 500 || document.documentElement.scrollTop >300) {
+        topBtn.classList.add('topActive');
+    } else {
+        topBtn.classList.remove('topActive');
+    }
+}
+
+window.onscroll = function() {
+    scrollBtn();
+}
+
+
+topBtn.addEventListener('click',() => {
+    window.scrollTo({top : 0, behavior: 'smooth'});
+})
+
+
+
---
+++ public/js/sub_noticeList.js
@@ -0,0 +1,19 @@
+// TOP버튼
+let topBtn = document.querySelector('#topBtn');
+
+function scrollBtn() {
+    if(document.body.scrollTop > 500 || document.documentElement.scrollTop >300) {
+        topBtn.classList.add('topActive');
+    } else {
+        topBtn.classList.remove('topActive');
+    }
+}
+
+window.onscroll = function() {
+    scrollBtn();
+}
+
+
+topBtn.addEventListener('click',() => {
+    window.scrollTo({top : 0, behavior: 'smooth'});
+})
