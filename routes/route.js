bd2b9ac6cb3ced48f000f0e21246f9457d402593
daeuniJ <daeunjeong161@gmail.com>
2022-12-14 11:56:55 +0900

    정다은_mysql 테이블 생성

--- routes/route.js
+++ routes/route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const { title } = require('process');
+const db = require('./../db.js');
 
 router.get('/', (req, res)=>{ /* 메인페이지 */
   res.render('mainPage');
