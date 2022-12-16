e2627e2def39b5d225aa24d742d8b36b5a280909
JEONG-JI-HEON <wlgjs0804@naver.com>
2022-12-16 10:25:22 +0900

    유효성검사 모두 되게 수정완료

--- public/js/join2Page.js
+++ public/js/join2Page.js
@@ -1,167 +1,141 @@
-function validate() {
-  //event.preventDefault();
-  var objID = document.getElementById("user_id");
-  var objPwd1 = document.getElementById("user_pw");
-  var objPwd2 = document.getElementById("user_pwCheck");
-  var objEmail = document.getElementById("user_mail");
-  var objName = document.getElementById("user_name");
-  var objNum1 = document.getElementById("my_num1");
-  var objNum2 = document.getElementById("my_num2");
-
-  var arrNum1 = new Array();
-  var arrNum2 = new Array();
-
-  var tempSum = 0; //주민번호 합을 넣는 공간
-
-  //아이디와 패스워드 값 데이터 정규화 공식
-  var regul1 = /^[a-zA-Z0-9]{4,12}$/;
-  //이메일 값 데이터 유효성 체크
-  //[]안에 있는 값만 쓰겠다
-
-  //이메일 정규화 공식
-  var regul2 = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/;
-  //이름 정규화 공식
-  var regul3 = /^[가-힝a-zA-Z]{2,}$/;
-  //var email = RegExp(/^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/);
-
-  //아이디 입력 하지 않았을 경우
-  if ((objID.value) == "") {
-      alert("아이디를 입력하지 않았습니다.");
-      objID.focus();
-      return false;
-  }
-  //아이디 유효성 검사
-  //내가 입력한 데이터를 검사하는 check()
-  //만약 내가 아이디에 정규화 방식을 하나라도 지키지 않으면 if문 안으로 들어가서 alert message를 띄움
-  if (!check(regul1, objID, "아이디는 4~12자의 대소문자와 숫자로만 입력 가능합니다.")) {
-      return false; //반환 할 곳 없이 if문 탈출
-  }
-  //비밀번호 입력 하지 않았을 경우
-  if ((objPwd1.value) == "") {
-      alert("비밀번호를 입력해 주세요");
-      objPwd1.focus();
-      return false;
-  }
-  if ((objPwd2.value == "")) {
-      alert("비밀번호를 입력해 주세요");
-      objPwd2.focus();
-      return false;
-  }
-
-  //비밀번호 유효성 검사
-  //만약 내가 비밀번호에 정규화 방식을 하나라도 지키지 않으면 if문 안으로 들어가서 alert message를 띄움
-  if (!check(regul1, objPwd1, "비밀번호는 4~12자의 대소문자와 숫자로만 입력 가능합니다.")) {
-      return false;
-  }
-
-  //비밀번호와 비밀번호 확인이 일치 하지 않을 경우
-  if ((objPwd1.value) != (objPwd2.value)) {
-      alert("비밀번호가 일치 하지 않습니다.");
-      objPwd1.focus();
-      objPwd2.focus();
-      return false;
-  }
-  //이메일 입력 안했을 경우
-  if ((objEmail.value) == "") {
-      alert("이메일을 입력해 주세요");
-      objEmail.focus();
-      return false;
-  }
-  //이메일이 잘못된 경우
-  if (!check(regul2, objEmail, "이메일을 잘못 입력 했습니다.")) {
-      return false;
-  }
-  //이름 입력 안 한 경우
-  if ((objName.value) == "") {
-      alert("이름을 입력해 주세요");
-      objName.focus();
-      return false;
-  }
-  //이름에 특수 문자가 들어간 경우
-  if (!check(regul3, objName, "이름이 잘못 되었습니다.")) {
-      return false;
-  }
-
-  //주민번호 앞자리를 각 배열에 넣어 검색 및 비교하기위한 단계
-  //앞자리 값 만큼 돌림
-  for (var i = 0; i < objNum1.value.length; i++) {
-      //배열 칸마다 앞자리 값 하나씩 넣어줌
-      arrNum1[i] = objNum1.value.charAt(i);
-  }
-
-  //주민번호 뒷자리를 각 배열에 넣어 검색 및 비교하기위한 단계
-  //뒷자리 값 만큼 돌림
-  for (var i = 0; i < objNum2.value.length; i++) {
-      //배열 칸마다 뒷자리 값 하나씩 넣어줌
-      arrNum2[i] = objNum2.value.charAt(i);
-  }
-  //주민번호 앞자리를 tempSum에 집어 넣기
-  //앞자리 만큼 돌림
-  for (var i = 0; i < objNum1.value.length; i++) {
-      //tempSum에 앞자리를 연이어 넣어줌
-      tempSum += arrNum1[i] * (2 + i);
-  }
-  //주민번호 뒷자리를 tempSum에 집어 넣기
-  //뒷자리 -1을 해주는 이유 : 뒷자리 마지막 자리는 더할 필요 없어서
-  //뒷자리의 -1만큼 돌림
-  for (var i = 0; i < objNum2.value.length - 1; i++) {
-      //뒷자리 2번째 자리 부터 
-      if (i >= 2) {
-          //tempSum에 2~6번째까지 넣어줌
-          tempSum += arrNum2[i] * i;
-      } else { //뒷자리 0~1번째 자리
-          //tempSum에 0~1번째까지 넣어줌
-          tempSum += arrNum2[i] * (8 + i);
-      }
-  }
-  //주민번호 유효성 체크
-  //주민번호 구하는 식
-  if (((11 - (tempSum % 11)) % 10) != arrNum2[6]) {
-      alert("올바른 주민 번호가 아닙니다.");
-      objNum1.value = "";
-      objNum2.value = "";
-      objNum1.focus();
-      return false;
-  }
-  //주민번호를 입력하면 생년월일이 자동으로 입력된다.
-  //substring은 시작문자와 끝문자를 검색하는 역할
-  if (arrNum2[0] == 1 || arrNum2[0] == 2) { //1900년생 일때
-      var y = parseInt(objNum1.value.substring(0, 2));
-      var m = parseInt(objNum1.value.substring(2, 4));
-      var d = parseInt(objNum1.value.substring(4, 6));
-      //년도,월,일을 각각 구함
-      f.my_year.value = 1900 + y; //년도
-      f.month.value = m; //월
-      f.day.value = d; //일
-  } else if (arrNum2[0] == 3 || arrNum2[0] == 4) { //2000년생 일때
-      var y = parseInt(objNum1.value.substring(0, 2));
-      var m = parseInt(objNum1.value.substring(2, 4));
-      var d = parseInt(objNum1.value.substring(4, 6));
-
-      f.my_year.value = 2000 + y; //년도
-      f.month.value = m; //월
-      f.day.value = d; //일
-  }
-  //관심분야가 하나라도 체크 되지 않은 경우
-  if (f.chk[0].checked == false && f.chk[1].checked == false &&
-      f.chk[2].checked == false && f.chk[3].checked == false &&
-      f.chk[4].checked == false) {
-      alert("관심분야를 체크해 주세요");
-      return false;
-  }
-  //자기소개 1글자라도 적지 않은 경우
-  if (f.my_intro.value == "") {
-      alert("자기소개를 입력해주세요");
-      return false;
-  }
+function checkAll() {
+    // 이름 체크 함수 호출
+    if (!checkName(joinform.user_name.value)) {
+        return false;
+    }
+    // 생년월일 체크 함수 호출
+    else if (!checkNull(joinform.user_birth.value, '생년월일를')) {
+        return false;
+    }
+    // 계좌번호 체크 함수 호출
+    else if (!checkNull(joinform.account_id.value, '계좌번호를')) {
+        return false;
+    }
+    // 계좌비밀번호 체크 함수 호출
+    else if (!checkNull(joinform.account_pw.value, '계좌 비밀번호를')) {
+        return false;
+    }
+    // id 체크 함수 호출
+    else if (!checkId(joinform.user_id.value)) {
+        return false;
+    }
+    // pw 체크 함수 호출
+    else if (!checkPw(joinform.user_id.value, joinform.user_pw.value, joinform.user_pwCheck.value)) {
+        return false;
+    }
+    // 우편번호 체크 함수 호출
+    else if (!checkNull(joinform.zipCode.value, '우편번호를')) {
+        return false;
+    }
+    // 기본주소 체크 함수 호출
+    else if (!checkNull(joinform.user_address.value, '기본주소를')) {
+        return false;
+    }
+    // 상세주소 체크 함수 호출
+    else if (!checkNull(joinform.user_addressMore.value, '상세주소를')) {
+        return false;
+    }
+    // 휴대전화 체크 함수 호출
+    else if (!checkNull(joinform.user_phoneNum.value, '휴대전화를')) {
+        return false;
+    }
+    // 이메일 체크 함수 호출
+    else if (!checkNull(joinform.user_mail.value, '이메일을')) {
+        return false;
+    }
+    // 회원가입 완료
+    alert("회원가입에 성공하였습니다.");
 }
 
-function check(re, what, message) { //정규화데이터,아이템 id,메세지
-  if (re.test(what.value)) { //what의 문자열에 re의 패턴이 있는지 나타내는 함수 test
-      //만약 내가 입력한 곳의 값이 정규화 데이터를 썼다면 true를 반환해서 호출 된 곳으로 리턴됨
-      return true;
-  }
-  alert(message);
-  what.value = "";
-  what.focus();
+// 공백 체크 함수
+function checkNull(value, data) {
+    // 유효성 검사
+    if (value == "") {
+        alert(data + " 입력해주세요.");
+        return false;
+    }
+    return true;
+}
+
+// id 체크 함수
+function checkId(id) {
+    // 공백 체크 함수 호출
+    if (!checkNull(id, '아이디를')) return false;
+
+    // 정규표현식 사용
+    var idRegExp = /^[a-zA-z0-9]{4,12}$/;
+
+    // 아이디 유효성 검사
+    if (!idRegExp.test(id)) {
+        alert("아이디는 영문 대소문자와 숫자 4~12자리로 입력하세요.");
+        // 값을 초기화
+        joinform.user_id.value = "";
+        // 마우스 커서 초점을 맞춰줌
+        joinform.user_id.focus();
+        return false;
+    }
+    return true;
+}
+
+// pw 체크 함수
+function checkPw(id, pw, pw2) {
+    // 공백 체크 함수 호출
+    if (!checkNull(pw, "비밀번호를")) return false;
+    if (!checkNull(pw2, "비밀번호 확인을")) return false;
+
+    // 정규표현식 사용
+    var pwRegExp = /^[a-zA-z0-9]{4,12}$/;
+
+    // 비밀번호 유효성 검사
+    if (!pwRegExp.test(pw)) {
+        alert("비밀번호는 영문 대소문자와 숫자 4~12자리로 입력하세요.");
+        // 값을 초기화
+        joinform.user_pw.value = "";
+        // 마우스 커서 초점을 맞춰줌
+        joinform.user_pw.focus();
+        return false;
+    }
+
+    // 비밀번호와 비밀번호 확인 값이 일치하지 않은경우
+    if (pw != pw2) {
+        alert("두 비밀번호가 일치하지 않습니다.");
+        // 값을 초기화
+        joinform.user_pw.value = "";
+        joinform.user_pwCheck.value = "";
+        // 마우스 커서 초점을 맞춰줌
+        joinform.user_pwCheck.focus();
+        return false;
+    }
+
+    // 아이디와 비밀번호가 일치한 경우
+    if (id == pw) {
+        alert("아이디와 비밀번호가 동일합니다.");
+        // 값을 초기화
+        joinform.user_pw.value = "";
+        joinform.user_pwCheck.value = "";
+        // 마우스 커서 초점을 맞춰줌
+        joinform.user_pw.focus();
+        return false;
+    }
+    return true;
+}
+
+
+// 이름 체크 함수
+function checkName(name) {
+    // 공백 체크 함수 호출
+    if (!checkNull(name, "이름을")) return false;
+
+    // 정규표현식 사용
+    var nameRegExp = /^[가-힣]{2,4}$/;
+    // 이름 유효성 검사
+    if (!nameRegExp.test(name)) {
+        alert("이름이 올바르지 않습니다.");
+        // 값을 초기화
+        joinform.user_name.value = "";
+        // 마우스 커서 초점을 맞춰줌
+        joinform.user_name.focus();
+        return false;
+    }
+    return true;
 }
