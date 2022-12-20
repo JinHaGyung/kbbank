9d5731f1c15211a53a2660474715bd19d5b5db9f
daeuniJ <daeunjeong161@gmail.com>
2022-12-20 10:08:23 +0900

    정다은_수정

--- public/js/siteMapPage.js
+++ public/js/siteMapPage.js
@@ -1,28 +1,6 @@
-//사이트맵 헤더 모션
-let siteMapPage_Hwrap = document.querySelector('.siteMapPage_Hwrap');
-let sitemapH_height = siteMapPage_Hwrap.offsetHeight;
-let kbLogo = document.querySelector('.kbLogo');
-let closeIcon = document.querySelector('.siteMap_header>a');
-
-
-window.addEventListener('scroll',() => {
-    let windowTop = window.scrollY;
-
-    if(windowTop > sitemapH_height) {
-        siteMapPage_Hwrap.classList.add('active');
-        kbLogo.classList.add('active');
-        closeIcon.classList.add('active');
-    } else {
-        siteMapPage_Hwrap.classList.remove('active');
-        kbLogo.classList.remove('active');
-        closeIcon.classList.remove('active');
-    }
-})
-
-
 //사이트맵 tab
+let tabContent = document.getElementsByClassName('sm_tabcont');
 function SM_tabOpen(e, tabName) {
-    let tabContent = document.getElementsByClassName('sm_tabcont');
     for(i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = 'none';
     }
