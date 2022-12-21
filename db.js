6bfa5edf8eabc50ab0e12035b1d2d6dc7aaa7578
daeuniJ <daeunjeong161@gmail.com>
2022-12-21 17:35:19 +0900

    정다은_메인페이지 공지사항 리스트

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '1234',
+  password : '95052910',
   database : 'kbbank',
   dateStrings : 'date'
 });
@@ -15,7 +15,7 @@ connection.connect(function(err) {
 
   //메인페이지 공지사항을 추출할 때
 function mainPageNoti(callback) {
-  connection.query(`SELECT * FROM kbnotice where date <= DATE_FORMAT(NOW(),'%Y-%m-%d') LIMIT 4`, (err, rows) => {
+  connection.query(`SELECT * FROM kbnotice where date <= DATE_FORMAT(NOW(),'%Y-%m-%d') ORDER BY date desc LIMIT 4`, (err, rows) => {
       if(err) throw err;
       callback(rows);
   })
