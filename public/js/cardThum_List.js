716be4bfa572ed753d850cfc9a87fc8316480c85
daeuniJ <daeunjeong161@gmail.com>
2022-12-27 12:43:23 +0900

    정다은_수정

---
+++ public/js/cardThum_List.js
@@ -0,0 +1,22 @@
+function filter(){
+
+    var value, name, item, i;
+
+    value = document.getElementById("value").value.toUpperCase();
+    item = document.getElementsByClassName("item");
+
+    for(i=0;i<item.length;i++){
+      name = item[i].getElementsByClassName("cardName");
+      if(name[0].innerHTML.toUpperCase().indexOf(value) > -1){
+        item[i].style.display = "flex";
+      }else{
+        item[i].style.display = "none";
+      }
+    }
+  }
+
+
+//카드상품 총 게시물 수
+  let cardThum_tol = document.querySelector('#cardThum_tol');
+  let card_li = document.getElementsByClassName('card_li');
+  cardThum_tol.innerText = card_li.length;
