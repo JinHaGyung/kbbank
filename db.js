0bec58789fc4332c24ca45f38f5b087503b9383f
JinHaGyung <wlsgkrud96@gmail.com>
2022-12-28 09:53:17 +0900

    카드조회 수정

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '95052910',
+  password : '1234',
   database : 'kbbank',
   dateStrings : 'date',
   multipleStatements: true
